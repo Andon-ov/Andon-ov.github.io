4177de296a6af9d8ce66a05cdea70cb4e25cda04
Andon-ov <bloodtaint@yahoo.com>
2024-02-22 21:57:08 +0200

    looking for a way to solve a problem with angular 17 and github pages (in angular 17 build folder all content is in browser folder so it can't launch github pages properly)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './core/home/home.component';
+// import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  // },
 ];
 
 @NgModule({
