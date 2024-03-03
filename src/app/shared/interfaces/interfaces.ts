c11ce43ac04bfa327f31c1f1be0677ad3b17029d
Andon-ov <bloodtaint@yahoo.com>
2024-03-03 17:56:54 +0200

    editing nav search style

--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -12,3 +12,60 @@ export interface Recipe {
   title: string;
   subtitle?: string;
 }
+
+/*
+created at
+edit at
+like 
+comment
+
+image can be a array of images
+  and max size 500kb when upload to cloud
+
+video link
+
+fork description to 
+
+  Necessary products
+    like a ingredients
+
+  Method of preparation
+
+
+*/ 
+
+
+/*
+user
+
+username / email
+password
+
+login wit facebook or google
+
+acc info
+edit my acc
+
+forgot password
+change password
+
+remove favorite recipe
+edit and delete my recipe
+
+comments 
+  edit / delete comments
+
+
+favorite recipe
+my recipe
+
+*/ 
+
+
+/*
+search 
+ can searching in title and ingredient name 
+
+
+ can add drop menu with search when push the button search
+*/ 
