bb58a578e6d3a750ee31f61d761bfa1ca6f5286d
Andon-ov <bloodtaint@yahoo.com>
2024-03-10 11:13:22 +0200

    edit recipes list css style

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,31 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {Recipe} from 'src/app/shared/interfaces/interfaces';
-import {RecipesService} from 'src/app/shared/services/recipes.service';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/shared/interfaces/interfaces';
+import { RecipesService } from 'src/app/shared/services/recipes.service';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
-
   recipes: Recipe[] = [];
 
-
-  constructor(private recipeService: RecipesService) {
-
-  }
+  constructor(private recipeService: RecipesService) {}
 
   ngOnInit(): void {
     this.getRecipes();
-
   }
 
   getRecipes(): void {
     this.recipeService.getRecipes().subscribe({
       next: (data) => {
-        this.recipes = data
-          .sort((a, b) => a.title.localeCompare(b.title));
+        this.recipes = data.sort((a, b) => a.title.localeCompare(b.title));
         // .filter((a) => a.is_active);
       },
       error: (error) => {
@@ -33,5 +27,4 @@ export class RecipesListComponent implements OnInit {
       },
     });
   }
-
 }
