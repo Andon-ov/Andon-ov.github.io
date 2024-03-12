cfcd814f07d70410798e5c2f6a245513f2c36da3
Andon-ov <bloodtaint@yahoo.com>
2024-03-12 20:42:38 +0200

    adding scroll-to-top component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,18 +5,21 @@ import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { RouterModule } from '@angular/router';
 import { SafeUrlPipe } from './pipe/safe-url.pipe';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
     CarouselComponent,
     ImageUploadComponent,
     SafeUrlPipe,
+    ScrollToTopComponent,
   ],
   imports: [CommonModule, EditorModule, RouterModule],
   exports: [
     CarouselComponent,
     ImageUploadComponent,
     SafeUrlPipe,
+    ScrollToTopComponent
   ],
 })
 export class SharedModule {}
