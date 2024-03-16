981911ec3460e0afde77085238f9163d433199a3
Andon-ov <bloodtaint@yahoo.com>
2024-03-16 07:07:48 +0200

    adding user guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { UserRecipesComponent } from './user/user-recipes/user-recipes.component
 import { DashboardComponent } from './user/dashboard/dashboard.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { CommentFormEditComponent } from './comment/comment-form-edit/comment-form-edit.component';
+import { userGuard } from './user/user.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: CarouselComponent },
@@ -62,6 +63,7 @@ const routes: Routes = [
   {
     path: 'recipe/:id',
     component: RecipeComponent,
+    canActivate:[userGuard]
   },
 
   { path: 'comment-edit/:id', component: CommentFormEditComponent },
