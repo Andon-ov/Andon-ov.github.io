629488223a490227db07eb3fabd87b696c62bb8a
Andon-ov <bloodtaint@yahoo.com>
2024-04-24 06:29:31 +0300

    adding animate.css and editing carousel component

--- src/app/public/carousel/carousel.component.ts
+++ src/app/public/carousel/carousel.component.ts
@@ -1,42 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
-
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+  autoplayTimeout: number = 7000;
+
+  slidesData = [
+    'https://learningherbs.com/wp-content/uploads/2019/08/herbal-first-aid-ointment-01.jpg',
+
+    'https://www.aveeno.com/sites/aveeno_us_2/files/healingherbswithmortaran_687349.jpg',
+
+    'https://cf.ltkcdn.net/diet-and-nutrition/natural-health-remedies/images/orig/298512-2096x1397-herbal-medicine.jpg',
+
+    'https://joanmorais.com/wp-content/uploads/2023/03/herbal-extract.jpg',
+
+    'https://imgix-prod.sgs.com/-/media/sgscorp/images/connectivity-and-products/aromatics-pexels.cdn.en-MU.1.jpg',
+  ];
+
+  backgroundColors = [
+    'rgba(40, 167, 69, 0.8)',
+    'rgba(167, 87, 94, 0.8)',
+    'rgba(155, 43, 42, 0.8)',
+    'rgba(162, 161, 0, 0.8)',
+    'rgba(137, 153, 182, 0.8)',
+  ];
+  currentBackgroundIndex = 0;
 
   customOptions: OwlOptions = {
-    loop: true,
-    margin:10,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
     dots: false,
-    autoplay: true, 
-    autoplayTimeout: 5000,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    items: 1,
+    smartSpeed: 450,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: this.autoplayTimeout,
     navSpeed: 700,
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
     },
-    nav: true
+    nav: false,
+  };
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.currentBackgroundIndex =
+        (this.currentBackgroundIndex + 1) % this.backgroundColors.length;
+    }, this.autoplayTimeout);
   }
-
-
-
 }
